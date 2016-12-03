959cb7577f453a403338716326afc131f178f654
PliskinIroquois <m.gs.0@hotmail.com>
2016-12-03 01:00:27 -0600

    Creacion de login, cambios en la DB
    Creacion del login, index e ingreso de usuario predeterminado en la DB.
    Estructuralmente la DB sigue igual pero se ingreso un usuario para
    probar.

--- proyecto_p4.sql
+++ proyecto_p4.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 02-12-2016 a las 03:17:22
+-- Tiempo de generación: 03-12-2016 a las 06:59:07
 -- Versión del servidor: 5.6.20-log
--- Versión de PHP: 5.6.18
+-- Versión de PHP: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -34,7 +34,8 @@ CREATE TABLE `hoteles` (
   `direccion` varchar(255) NOT NULL,
   `telefono` varchar(12) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `ubicacionFotografia` varchar(120) NOT NULL
+  `ubicacionFotografia` varchar(120) NOT NULL,
+  `IdH` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,11 +45,13 @@ CREATE TABLE `hoteles` (
 --
 
 CREATE TABLE `tours` (
-  `nombreT` varchar(45) NOT NULL,
-  `descripcion` varchar(255) NOT NULL,
-  `empresa` varchar(45) NOT NULL,
+  `nombreTour` varchar(45) NOT NULL,
+  `descripcionTour` varchar(255) NOT NULL,
+  `nombreEmpresa` varchar(45) NOT NULL,
   `telefono` varchar(12) NOT NULL,
-  `email` varchar(25) NOT NULL
+  `email` varchar(25) NOT NULL,
+  `IdTour` int(11) NOT NULL,
+  `ubicacionTour` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -59,15 +62,58 @@ CREATE TABLE `tours` (
 
 CREATE TABLE `usuarios` (
   `NIF` varchar(12) NOT NULL,
-  `idUser` varchar(10) NOT NULL,
-  `nombreU` varchar(45) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `nombreUsuario` varchar(45) NOT NULL,
   `apellidos` varchar(92) NOT NULL,
   `email` varchar(25) NOT NULL,
   `telefono` varchar(12) NOT NULL,
   `clave` varchar(30) NOT NULL,
-  `userType` int(11) NOT NULL
+  `userType` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`NIF`, `username`, `nombreUsuario`, `apellidos`, `email`, `telefono`, `clave`, `userType`) VALUES
+('123', 'root', '', '', '', '', 'admin', 'admin');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  ADD PRIMARY KEY (`IdH`);
+
+--
+-- Indices de la tabla `tours`
+--
+ALTER TABLE `tours`
+  ADD PRIMARY KEY (`IdTour`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`NIF`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  MODIFY `IdH` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tours`
+--
+ALTER TABLE `tours`
+  MODIFY `IdTour` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
